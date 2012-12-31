1e95a393085721ea3d2b7d05a282b4ffd47ea622
Chris Dwan <radixhound@gmail.com>
2012-12-31 09:40:07 -0800

    Initial project, nothing here yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBilling::Application.config.secret_token = '11a0a2e6b6fd7086ad74713fd21f022df9b97e7a5994815c2bef3abb5514b13cb18c2897face82fead445d82ee29c87747f37f65c11869f6a7018b2733569a12'
