05ecca06d7c35b53218ef60d6c4df4b378cf10a5
Chris Dwan <radixhound@gmail.com>
2013-01-10 22:13:49 -0800

    Fix UserActivator to handle password-only activations'

--- features/support/timeout.rb
+++ features/support/timeout.rb
@@ -1,6 +1,6 @@
 def retry_on_timeout(n = 3, &block)
   block.call
-rescue Capybara::TimeoutError, Capybara::ElementNotFound => e
+rescue Exception => e
   if n > 0
     puts "Catched error: #{e.message}. #{n-1} more attempts."
     retry_on_timeout(n - 1, &block)
